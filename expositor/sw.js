119b11b449dc14e545fd65eb6b813c72e4ef9f8a
lojaluzmariana <contatoluzmariana@gmail.com>
2026-09-24 21:17:29 -0300

    Add files via upload

---
+++ expositor/sw.js
@@ -0,0 +1,5 @@
+const CACHE='luz-mariana-expositor-v1';
+const SHELL=['./','./index.html','./manifest.webmanifest','./logo-luz-mariana.png','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./qr-instagram.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).catch(()=>{}))});
+self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==self.location.origin)return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
